2b37c721562154a946931d991d9979e62d4df0bd
Dennis Ryumin <d.ryumin@sigma-it.ru>
2022-01-24 22:13:44 +0300

    add simple page controllers to MVC project

--- MVC/src/main/java/com/katapios/springexamples/mvc/HelloController.java
+++ MVC/src/main/java/com/katapios/springexamples/mvc/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/hello")
+    @GetMapping("/hello-world")
     public String sayHello(){
         return "hello_world";
     }
---
+++ MVC/src/main/java/com/katapios/springexamples/mvc/controllers/FirstController.java
@@ -0,0 +1,25 @@
+package com.katapios.springexamples.mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+//@RequestMapping("/first")
+public class FirstController {
+    @GetMapping("/hello")
+    public String helloPage(@RequestParam(value = "name", required = false) String name,
+                            @RequestParam(value = "surname", required = false) String surname) {
+
+        System.out.println("name = " + name + " surname = " + surname);
+        return "first/hello";
+    }
+
+    @GetMapping("/goodbye")
+    public String goodByePage() {
+        return "first/goodbye";
+    }
+}
