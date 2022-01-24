5568c1a2c3cf8cb5386d82390f3342220a657691
Dennis Ryumin <d.ryumin@sigma-it.ru>
2022-01-24 17:45:25 +0300

    init MVC project

---
+++ MVC/src/main/java/com/katapios/springexamples/mvc/HelloController.java
@@ -0,0 +1,12 @@
+package com.katapios.springexamples.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello_world";
+    }
+}
